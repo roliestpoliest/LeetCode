0ebf07532d10dc2d8e4f57b9a4d47385caf84589
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2023-07-04 11:55:28 -0400

    🧹Reorganize Folders

--- Arrays & Hashing/217/ContainsDuplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        freq = {} # num : val
-        for n in nums:
-            freq[n] = 1 + freq.get(n, 0)
-            if freq[n] > 1:
-                return True
-        return False
